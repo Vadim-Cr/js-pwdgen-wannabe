8df5bb16a3d4f03edcfd34fed35910b2db1922a6
Vadim-Cr <vadim.crudu@gmail.com>
2023-04-26 16:11:15 +0200

    finish 2

--- password.js
+++ password.js
@@ -1,5 +1,5 @@
 // Dichiaro le mie variabili
-let userName, userSurname, userFavColor, concatena, messaggio
+let userName, userSurname, userFavColor, userAge, concatena, messaggio
 
 // chiedo all'utente il suo nome
 userName = prompt("Quando sei stato battezzato che nome ti hanno affibbiato?")
@@ -7,8 +7,10 @@ userName = prompt("Quando sei stato battezzato che nome ti hanno affibbiato?")
 userSurname = prompt("All'anagrafe con quale disgraziato nome di famiglia sei stato maledetto?")
 // chiedo all'utente il suo colore preferito
 userFavColor = prompt("Tanto non ce ne importa del colore che scrivi qui, sicuramnete sarà qulacosa di banale")
+// chiedo all'utente la sua età
+userAge = prompt("Sottrai gli anni che ti rimangono all'età della tua morte e inserisci qui il risultato!")
 // concatena i risultati
-concatena = userName.concat(userSurname,userFavColor, "21!")
+concatena = userName.concat(userSurname, userFavColor, userAge, "!")
 // scrivi il messaggio
 messaggio = `La tua password super sicura e poco banale è: ${concatena}`
 
